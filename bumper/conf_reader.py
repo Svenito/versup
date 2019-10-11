fac3fdf3d122312813f1c641db2681d8c2dca676
Sven Steinbauer <sven@unlogic.co.uk>
2019-10-11 14:55:24 +0100

    Add support for global config

--- bumper/conf_reader.py
+++ bumper/conf_reader.py
@@ -3,23 +3,27 @@ import json
 from bumper.default_conf import default_conf
 
 
-def parse_config_file():
+config_files = ["./bumper.json", "~/.bumper.json"]
+
+
+def parse_config_file(config_file):
     cwd = os.getcwd()
-    with open(os.path.join(cwd, "bumper.json"), "r") as conf_file:
-        d = json.loads(conf_file.read())
+    d = {}
+    try:
+        with open(os.path.expanduser(config_file), "r") as conf_file:
+            d = json.loads(conf_file.read())
+    except FileNotFoundError:
+        pass
     return d
 
 
-def merge_config_with_default():
+def merge_configs_with_default():
     # TODO: Add support for user, local, and dynamic settings
     current_config = default_conf
-    try:
-        config = parse_config_file()
-    except:
-        return current_config
+    for config_file in config_files:
+        config = parse_config_file(config_file)
+        current_config = {**current_config, **config}
 
-    for k in config.keys():
-        current_config[k] = config[k]
     return current_config
 
 
